2c0062a0c77054cddaa4d4ddf500a0d795d2a190
dretam <tamaskywalker2@gmail.com>
2020-08-26 11:17:36 +0700

    menambah paragraf kedua

--- index.php
+++ index.php
@@ -10,6 +10,7 @@
 <body>
     <h1>Hello World!</h1>
     <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Doloribus quos, quidem aliquid delectus nisi distinctio rerum porro. Animi inventore maiores sequi officia. Aliquam sed consectetur quod non eum vel labore!</p>
+    <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Asperiores, a?</p>
 </body>
 
 </html>
